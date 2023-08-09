5eca86d2b65a780e1fec4da4e2dcc2258d75fdb8
Ekramuddin Ansari <ekramhabib27@gmail.com>
2023-08-09 08:56:28 +0530

    AnonymousFunctionAndLambdaExpression

---
+++ AnnonymousFunctionAndLambdaExpressionStudy/Example.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnonymousFunctionAndLambdaExpressionStudy
+{
+    public delegate void MyDelegate(string Data);
+    
+    internal class Example
+    {
+        //public static void ShowData(string Data)
+        //{
+        //    Console.WriteLine(Data);
+
+        //}
+        public MyDelegate ObjMyDelegate = delegate (string Data)
+        {
+            Console.WriteLine(Data);
+        };
+        public Action<string> ObjMyActionDelegate = delegate (string Data)
+        {
+            Console.WriteLine(Data);
+        };
+
+        //Lambda expression 
+
+        public Action<string> ObjMyActionLambda =  (Data)=> // => called lambda expression
+        {
+            Console.WriteLine(Data);
+        };
+
+
+
+
+    }
+
+}
---
+++ AnnonymousFunctionAndLambdaExpressionStudy/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using AnnonymousFunctionAndLambdaExpressionStudy;
+
+Example objExample = new Example();
+objExample.ObjMyDelegate.Invoke("Ekram");
+
+objExample.ObjMyActionDelegate.Invoke("Action Delegate Called");
+objExample.ObjMyActionLambda.Invoke("Action lambda called");
